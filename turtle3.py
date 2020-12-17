ee629c5b8c2bf63f0f95ac506d7184e1866ea057
Bartłomiej Musiał <61922476+bartlomiejmusial@users.noreply.github.com>
2020-12-17 15:05:27 +0100

    Create turtle3.py

---
+++ turtle3.py
@@ -0,0 +1,27 @@
+from turtle import Turtle, Screen, colormode
+from random import randint
+
+tim = Turtle()
+tim.shape("turtle")
+colormode(255)
+
+def rgb():
+    R = randint(0, 255)
+    G = randint(0, 255)
+    B = randint(0, 255)
+    tim.pencolor(R, G, B)
+
+
+# TURTLE DRAWING SHAPES WITH RANDOM GENERATED RGB COLORS
+for shape in range(3, 9):
+    rgb()
+    tim.forward(100)
+    for side in range(shape - 1):
+        tim.right(360/shape)
+        tim.forward(100)
+    tim.right(360 / shape)
+
+
+
+screen = Screen() 
+screen.exitonclick() 
