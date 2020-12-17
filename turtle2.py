245e8fa4cc4d3b22ea4a24fcd3a7f65a48623f41
Bartłomiej Musiał <61922476+bartlomiejmusial@users.noreply.github.com>
2020-12-17 15:04:01 +0100

    Create turtle2.py

---
+++ turtle2.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+
+# DRAW DASHED LINE
+for _ in range(30):
+    tim.forward(5)
+    tim.penup()
+    tim.forward(5)
+    tim.pendown()
+    
+    
+screen = Screen()
+screen.exitonclick()
