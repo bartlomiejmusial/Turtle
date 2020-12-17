6dbf335f85d579e67529a91a7ddf6e808e6fc14a
Bartłomiej Musiał <61922476+bartlomiejmusial@users.noreply.github.com>
2020-12-17 15:04:33 +0100

    Update turtle1.py

--- turtle1.py
+++ turtle1.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 
+
 # DRAW SQUARE
 tim = Turtle()
 tim.shape("turtle")
@@ -7,3 +8,7 @@ tim.color("DeepPink")
 for side in range(4):
     tim.forward(200)
     tim.right(90)
+
+    
+screen = Screen() 
+screen.exitonclick()
