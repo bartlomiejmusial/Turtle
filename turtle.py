d3ee79f5d3b081861ad55352bd4417e0419e935b
Bartłomiej Musiał <61922476+bartlomiejmusial@users.noreply.github.com>
2020-12-17 14:08:47 +0100

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,4 +1,13 @@
 from turtle import *
+
+# DRAW SQUARE
+# tim = Turtle()
+# tim.shape("turtle")
+# tim.color("DeepPink")
+# for side in range(4):
+#     tim.forward(200)
+#     tim.right(90)
+
 color('MidnightBlue', 'gold1')
 begin_fill()
 while True:
