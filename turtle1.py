e01859fa9effc6589126cf7a9df01808a5ba34f3
Bartłomiej Musiał <61922476+bartlomiejmusial@users.noreply.github.com>
2020-12-17 15:02:16 +0100

    Update turtle1.py

--- turtle1.py
+++ turtle1.py
@@ -1,18 +1,9 @@
 from turtle import Turtle, Screen
 
-# # DRAW SQUARE
-# tim = Turtle()
-# tim.shape("turtle")
-# tim.color("DeepPink")
-# for side in range(4):
-#     tim.forward(200)
-#     tim.right(90)
-
-# # DRAW DASHED LINE
-# for _ in range(30):
-#     tim.forward(5)
-#     tim.penup()
-#     tim.forward(5)
-#     tim.pendown()
-
-
+# DRAW SQUARE
+tim = Turtle()
+tim.shape("turtle")
+tim.color("DeepPink")
+for side in range(4):
+    tim.forward(200)
+    tim.right(90)
